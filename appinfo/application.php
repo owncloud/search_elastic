4fd56c0621c4470e20d802ad22bdf319cc15fdb3
VicDeo <victor.dubiniuk@gmail.com>
2019-04-01 15:59:20 +0300

    Move the app initialization from app into application

--- appinfo/application.php
+++ appinfo/application.php
@@ -16,6 +16,7 @@ namespace OCA\Search_Elastic\AppInfo;
 
 use OCA\Search_Elastic\Controller\AdminSettingsController;
 use OCA\Search_Elastic\Db\StatusMapper;
+use OCA\Search_Elastic\Hooks\Files;
 use OCA\Search_Elastic\SearchElasticService;
 use OCA\Search_Elastic\SearchElasticConfigService;
 use OCP\AppFramework\App;
@@ -24,8 +25,11 @@ use OCP\AppFramework\IAppContainer;
 class Application extends App {
 	const APP_ID = 'search_elastic';
 
+	/** @var bool */
+	private $isSearchProviderRegistered = false;
+
 	public function __construct(array $urlParams=[]) {
-		parent::__construct('search_elastic', $urlParams);
+		parent::__construct(self::APP_ID, $urlParams);
 
 		$container = $this->getContainer();
 
@@ -100,5 +104,80 @@ class Application extends App {
 				$c->query('SearchElasticService')
 			);
 		});
+	}
+
+	public function init() {
+		$server = $this->getContainer()->getServer();
+		$config = $server->getConfig();
+		$mode = $config->getAppValue(self::APP_ID, SearchElasticConfigService::APP_MODE, 'active');
+		if ($mode === 'active') {
+			$this->initFrontEnd();
+			$this->registerSearchProvider();
+			$this->registerHooks();
+			// add background job for deletion
+			$server->getJobList()->add(new \OCA\Search_Elastic\Jobs\DeleteJob());
+		}
+	}
+
+	public function registerSearchProvider() {
+		if ($this->isSearchProviderRegistered === true) {
+			return;
+		}
+
+		$server = $this->getContainer()->getServer();
+		$config = $server->getConfig();
+		$group = $config->getAppValue(self::APP_ID, SearchElasticConfigService::ENABLED_GROUPS, null);
+		if (empty($group) || (
+				$server->getUserSession()->getUser()
+				&& $server->getGroupManager()->isInGroup(
+					$server->getUserSession()->getUser()->getUID(), $group
+				)
+			)
+		) {
+			$this->isSearchProviderRegistered = true;
+			$server->getSearch()->removeProvider('OC\Search\Provider\File');
+			$server->getSearch()->registerProvider('OCA\Search_Elastic\Search\ElasticSearchProvider', ['apps' => ['files']]);
+		}
+	}
+	
+	private function initFrontEnd() {
+		\OCP\Util::addScript(self::APP_ID, 'search');
+		\OCP\Util::addStyle(self::APP_ID, 'results');
+	}
+	
+	private function registerHooks() {
+		$server = $this->getContainer()->getServer();
+		$eventDispatcher = $server->getEventDispatcher();
+		$fileHook = new Files();
+		$eventDispatcher->addListener('file.aftercreate', [$fileHook, 'contentChanged']);
+		$eventDispatcher->addListener('file.afterupdate', [$fileHook, 'contentChanged']);
+
+		//connect to the filesystem for rename
+		\OCP\Util::connectHook(
+			\OC\Files\Filesystem::CLASSNAME,
+			\OC\Files\Filesystem::signal_post_rename,
+			'OCA\Search_Elastic\Hooks\Files',
+			Files::handle_post_rename);
+
+		//listen for file shares to update read permission in index
+		\OCP\Util::connectHook(
+			'OCP\Share',
+			'post_shared',
+			'OCA\Search_Elastic\Hooks\Files',
+			Files::handle_share);
+
+		//listen for file un shares to update read permission in index
+		\OCP\Util::connectHook(
+			'OCP\Share',
+			'post_unshare',
+			'OCA\Search_Elastic\Hooks\Files',
+			Files::handle_share);
+
+		//connect to the filesystem for delete
+		\OCP\Util::connectHook(
+			\OC\Files\Filesystem::CLASSNAME,
+			\OC\Files\Filesystem::signal_delete,
+			'OCA\Search_Elastic\Hooks\Files',
+			Files::handle_delete);
 	}
 }
